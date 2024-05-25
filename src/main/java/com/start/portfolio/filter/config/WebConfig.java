72b67dc582e0d2fb48b3f20df00b9ecbbd5c831d
oooo91 <hyelneu29@gmail.com>
2024-05-26 03:30:44 +0900

    fix : resolver 등록

---
+++ src/main/java/com/start/portfolio/filter/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.start.portfolio.filter.config;
+
+import com.start.portfolio.util.resolver.CustomAuthUserResolver;
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	// TODO 리졸버 등록
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(new CustomAuthUserResolver());
+	}
+}
